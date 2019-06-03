8af6c2550ead5ed281928c1ba8c3aaf8aedb9b74
William Zhang <willzhang05@gmail.com>
2019-06-03 18:04:11 -0400

    initial working input status

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
-from celery import Celery
 import serial
 import json
 
--- static/js/client.js
+++ static/js/client.js
@@ -1,11 +1,32 @@
 $(document).ready(function() {
-    var namespace = '/test';
-    var socket = io.connect('http://' + document.domain + ':' + location.port + namespace);
-    socket.on('connect', function() {
+    var namespace = "/test";
+    var socket = io.connect("http://" + document.domain + ":" + location.port + namespace);
+    socket.on("connect", function() {
         console.log("connected!");
     });
-    socket.on('update', function(msg) {
-        console.log(msg.data);
-        $("#status").append('<p>' + msg.data + '</p>');
+    socket.on("update", function(msg) {
+        if (!("type" in msg.data)) {
+            console.log(msg.data);
+            return;
+        }
+        for (var key in msg.data) {
+            if (key === "" || key === "type") {
+                continue;
+            }
+            switch(msg.data["type"]) {
+                case "inputs":
+                    console.log(key);
+                    var colIndex = $("#input-headers").children().index($("#" + key));
+                    $("#input-values").children().eq(colIndex).html(msg.data[key]);
+                    //console.log($("#input-values").children().eq(colIndex));
+                    console.log(colIndex);
+                    //$("#" + key).html(msg.data[key]);
+                    break;
+                case "can0":
+                    break;
+                case "can1":
+                    break;
+            }
+        }
     });
 });
