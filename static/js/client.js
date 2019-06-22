99b48c78de7c6a339b7035ac488df0a0d3f98a6f
QuinnCiccoretti <quinnc423@gmail.com>
2019-06-22 01:10:40 -0400

    elegent

--- static/js/client.js
+++ static/js/client.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
-    // global, dynamic mapping of labels to value for left
-    var basic_options = {
+    //ATTENTION
+    //will cap the charts. They can auto-scale but its jank
+    //adjust to real values as needed    
+    var max_heights = [6000,1000,1000,50,25]
+    var chart_config = {
+        axisY:{
+            maximum: 100000
+        },
         axisX:{
             gridThickness: 0,
             tickLength: 0,
@@ -15,18 +21,21 @@ $(document).ready(function() {
             dataPoints: []
         }
         ]
-    };
+    }
+    $.each( $("#kls-l-values").children(), function(i){
+        console.log(i);
+        $( this ).html("<div style=\"height: 370px; width: 100%;\"></div>")
+        chart_config.axisY.maximum = max_heights[i];
+        console.log(chart_config.axisY)
+        $( this ).first().CanvasJSChart(chart_config);
+    });
+    $.each( $("#kls-r-values").children(), function(i){
+        $( this ).html("<div style=\"height: 370px; width: 100%;\"></div>")
+        chart_config.axisY.maximum = max_heights[i];
+        $( this ).first().CanvasJSChart(chart_config);
+    });
+    $(".canvasjs-chart-credit").remove();
 
-    var colindex = $("#kls-l-headers").children()
-    $.each( $("#kls-l-values").children(), function(){
-        $( this ).html("<div style=\"height: 370px; width: 100%;\"></div>")
-        $( this ).first().CanvasJSChart(basic_options);
-    });
-    var colindex = $("#kls-r-headers").children()
-    $.each( $("#kls-r-values").children(), function(){
-        $( this ).html("<div style=\"height: 370px; width: 100%;\"></div>")
-        $( this ).first().CanvasJSChart(basic_options);
-    });
 
     var namespace = "/test";
     var socket = io.connect("http://" + document.domain + ":" + location.port + namespace);
@@ -35,7 +44,7 @@ $(document).ready(function() {
     });
     socket.on("update", function(msg) {
         if (!("type" in msg.data)) {
-            console.log(msg.data);
+            // console.log(msg.data);
             return;
         }
         for (var key in msg.data) {
@@ -48,24 +57,26 @@ $(document).ready(function() {
                     $("#input-values").children().eq(colIndex).CanvasJSChart();
                     break;
                 case "kls_l":
-                    var colindex = $("#kls-l-headers").children().index($("#kls-l-" + key));
+                    var colindex_left = $("#kls-l-headers").children().index($("#kls-l-" + key));
                     
-                    $("#kls-l-values").children().eq(colindex).first().CanvasJSChart().data[0].set("dataPoints", 
+                    $("#kls-l-values").children().eq(colindex_left).first().CanvasJSChart().data[0].set("dataPoints", 
                         [{  y: msg.data[key] }]
                     );
-                     $("#kls-l-values").children().eq(colindex).first().CanvasJSChart().render();
+                     $("#kls-l-values").children().eq(colindex_left).first().CanvasJSChart().render();
                     
                     break;
                 case "kls_r":
-                    var colindex = $("#kls-r-headers").children().index($("#kls-r-" + key));
+                    var colindex_right = $("#kls-r-headers").children().index($("#kls-r-" + key));
                     
-                    $("#kls-r-values").children().eq(colindex).first().CanvasJSChart().data[0].set("dataPoints", 
+                    $("#kls-r-values").children().eq(colindex_right).first().CanvasJSChart().data[0].set("dataPoints", 
                         [{  y: msg.data[key] }]
                     );
-                     $("#kls-r-values").children().eq(colindex).first().CanvasJSChart().render();
+                     $("#kls-r-values").children().eq(colindex_right).first().CanvasJSChart().render();
                     
                     break;
             }
+                $(".canvasjs-chart-credit").remove();
+
         }
     });
 });
