85fb5fcc69a605c409545f49a8b428a6588d33f1
QuinnCiccoretti <quinnc423@gmail.com>
2019-06-22 01:23:16 -0400

    fixed bricky stuff

--- static/js/client.js
+++ static/js/client.js
@@ -3,36 +3,54 @@ $(document).ready(function() {
     //will cap the charts. They can auto-scale but its jank
     //adjust to real values as needed    
     var max_heights = [6000,1000,1000,50,25]
-    var chart_config = {
-        axisY:{
-            maximum: 100000
-        },
-        axisX:{
-            gridThickness: 0,
-            tickLength: 0,
-            lineThickness: 0,
-            labelFormatter: function(){
-              return " ";
-            }
-          },
-        data: [              
-        {
-            type: "column",
-            dataPoints: []
-        }
-        ]
-    }
     $.each( $("#kls-l-values").children(), function(i){
         console.log(i);
         $( this ).html("<div style=\"height: 370px; width: 100%;\"></div>")
-        chart_config.axisY.maximum = max_heights[i];
-        console.log(chart_config.axisY)
-        $( this ).first().CanvasJSChart(chart_config);
+        $( this ).first().CanvasJSChart(
+            {
+                axisY:{
+                    maximum: max_heights[i]
+                },
+                axisX:{
+                    gridThickness: 0,
+                    tickLength: 0,
+                    lineThickness: 0,
+                    labelFormatter: function(){
+                      return " ";
+                    }
+                  },
+                data: [              
+                {
+                    type: "column",
+                    dataPoints: []
+                }
+                ]
+            }
+        );
     });
     $.each( $("#kls-r-values").children(), function(i){
         $( this ).html("<div style=\"height: 370px; width: 100%;\"></div>")
-        chart_config.axisY.maximum = max_heights[i];
-        $( this ).first().CanvasJSChart(chart_config);
+        $( this ).first().CanvasJSChart(
+            {
+                axisY:{
+                    maximum: max_heights[i]
+                },
+                axisX:{
+                    gridThickness: 0,
+                    tickLength: 0,
+                    lineThickness: 0,
+                    labelFormatter: function(){
+                      return " ";
+                    }
+                  },
+                data: [              
+                {
+                    type: "column",
+                    dataPoints: []
+                }
+                ]
+            }
+        );
     });
     $(".canvasjs-chart-credit").remove();
 
